289d016370f8c0fc1c4e88b5620a533da142ebe2
Naveen Goyal <navgoyal@cisco.com>
2024-10-02 11:17:03 +0530

    add qr

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,9 +12,24 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+
+
 export const metadata = {
-  title: "Recharge App",
-  description: "Discount upto 95% on any recharge",
+  title: 'Recharge Your Mobile | Amazing Offers',
+  description: 'Recharge your mobile with the best offers available upto 95% discount. Choose a plan and pay easily using UPI!',
+  openGraph: {
+    title: 'Recharge Your Mobile',
+    description: 'Recharge your mobile with the best offers available. Choose a plan and pay easily using UPI!',
+    images: [
+      {
+        url: '/offer.png', // Replace with your image path in the public directory
+        width: 800,
+        height: 600,
+        alt: 'Recharge Offers',
+      },
+    ],
+    url: 'https://recharge-app-lime.vercel.app/', // Change this to your actual URL when deployed
+  },
 };
 
 export default function RootLayout({ children }) {
